c2924b08f53fee08c946c616d9fa344998039a8e
daniiskrr <danrr2001@gmail.com>
2023-03-30 18:02:32 +0200

    subida

---
+++ app/Http/Controllers/API/PostController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Posts;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    //Cojer todos los datos
+    public function index(){
+        $posts = Posts::all()->toArray();
+        return $posts;
+    }
+
+    public function add(Request $request){
+
+        $request->validate([
+                'name' => 'required',
+                'description' => 'required',
+                'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg,webp|max:2048',
+                'price' => 'required',
+            ]);
+        
+        $input = $request->all();
+        $imageName = NULL;
+
+        if($image = $request->file('file')){
+            $destinationPath = 'img/';
+            $imageName = date('YmdHis') . "." . $image->getClientOriginalExtension();
+            $image->move($destinationPath, $imageName);
+            $input['image'] = $imageName;
+        }
+
+        Posts::create($input);
+
+        return response()->json(['success' => 'Post creado correctamente']);
+    }
+}
