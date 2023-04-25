28c28f466e25c8479de758a4767f37d770fdccd1
daniiskrr <danrr2001@gmail.com>
2023-04-25 17:09:43 +0200

    xd

--- app/Models/Roles.php
+++ app/Models/Roles.php
@@ -1,18 +1,20 @@
 <?php
- 
+
 namespace App\Models;
- 
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
- 
-class User extends Model
+
+class Role extends Model
 {
+    use HasFactory;
+
     /**
      * The roles that belong to the user.
      */
-    public function roles(): BelongsToMany
+    public function users()
     {
-        return $this->belongsToMany(User::class, 'users_roles', 'id_rol', 'id_usuario');
+        return $this->belongsToMany(User::class, 'users_roles');
     }
 }
-?>
--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function roles(){
+        return $this->belongsToMany(Role::class, 'users_roles');
+    }
 }
